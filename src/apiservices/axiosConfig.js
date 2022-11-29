058f7e6b956c3b806dfbc1d8fdc82183b6f3d160
Gutovp1 <paivaav@gmail.com>
2022-11-29 17:27:07 -0300

    update base url local tunnel

--- src/apiservices/axiosConfig.js
+++ src/apiservices/axiosConfig.js
@@ -4,7 +4,9 @@ import axios from "axios";
 // axios.defaults.headers.common["Authorization"] =
 // localStorage.getItem("adminJwt");
 const instance = axios.create({
-  baseURL: "https://library-livre-api.azurewebsites.net/",
+  // baseURL: "https://library-livre-api.azurewebsites.net/",
+  // baseURL: "http://localhost:5001/api/",
+  baseURL: "https://tired-pears-do-177-24-156-179.loca.lt/",
   headers: {
     "Content-Type": "application/json",
     Authorization: localStorage.getItem("adminJwt"),
