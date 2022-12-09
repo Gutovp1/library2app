42bfc87bef066467b01a78d9b7422af435a99779
Gutovp1 <paivaav@gmail.com>
2022-12-09 08:31:06 -0300

    Url lt update

--- src/apiservices/axiosConfig.js
+++ src/apiservices/axiosConfig.js
@@ -5,8 +5,8 @@ import axios from "axios";
 // localStorage.getItem("adminJwt");
 const instance = axios.create({
   // baseURL: "https://library-livre-api.azurewebsites.net/",
-  // baseURL: "http://localhost:5001/api/",
-  baseURL: "https://free-bats-double-189-114-203-21.loca.lt/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "https://orange-readers-bow-189-114-203-21.loca.lt/api/",
   headers: {
     "Bypass-Tunnel-Reminder": "1",
     "Content-Type": "application/json",
