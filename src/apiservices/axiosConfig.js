4c99e9480b6d52b666efc6e6de1e7e9444d44427
Gutovp1 <paivaav@gmail.com>
2023-01-05 13:56:00 -0300

    Update axios config

--- src/apiservices/axiosConfig.js
+++ src/apiservices/axiosConfig.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/api/",
-  // baseURL: "https://nice-hotels-warn-177-24-157-123.loca.lt/api/",
-  // baseURL: "https://2nrrqslkbj.execute-api.sa-east-1.amazonaws.com/api/",
+  // baseURL: "http://localhost:5001/api/",
+  baseURL: " https://fancy-turkeys-add-191-251-91-200.loca.lt/api/",
   headers: {
-    // "Access-Control-Allow-Headers":
-    //   "Content-Type,X-Amz-Date,X-Amz-Security-Token,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-    // "Access-Control-Allow-Origin": "*",
-    // "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
     "Bypass-Tunnel-Reminder": "1",
     "Content-Type": "application/json",
     Authorization: localStorage.getItem("adminJwt"),
